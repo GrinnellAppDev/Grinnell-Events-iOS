fa99a1dea6ec81b98d25bb4552125408d743b275
Lea Marolt Sonnenschein <maroltso@grinnell.edu>
2013-09-08 14:35:31 -0500

    Initial commit @dr
    Xcode project created

---
+++ Grinnell-Events-iOS/Grinnell-Events-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Grinnell-Events-iOS
+//
+//  Created by Lea Marolt on 9/8/13.
+//  Copyright (c) 2013 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Grinnell-Events-iOS/Grinnell-Events-iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Grinnell-Events-iOS
+//
+//  Created by Lea Marolt on 9/8/13.
+//  Copyright (c) 2013 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
