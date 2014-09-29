280fdf629fe4fb998ca4e3168c90ab66f063c33b
alexthemitchell <mitchell17@grinnell.edu>
2014-09-29 10:36:34 -0500

    Add push functionality

--- Grinnell-Events-iOS/Grinnell-Events-iOS/AppDelegate.m
+++ Grinnell-Events-iOS/Grinnell-Events-iOS/AppDelegate.m
@@ -19,12 +19,22 @@
     NSDictionary *keysDict = [NSDictionary dictionaryWithContentsOfFile:strings_private];
     // Override point for customization after application launch.
     
-    [GAEvent registerSubclass]; 
+    
     // Set up Parse Grinnell Events credentials.
     [Parse setApplicationId:[keysDict objectForKey:@"ParseAppID"]
                   clientKey:[keysDict objectForKey:@"ParseClientKey"]];
-    
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                             categories:nil];
+    [application registerUserNotificationSettings:settings];
+    [application registerForRemoteNotifications];
+    
+    [GAEvent registerSubclass];
+    
     [Flurry setCrashReportingEnabled:NO];
     [Flurry startSession:[keysDict objectForKey:@"FlurrySession"]];
     
@@ -32,7 +42,19 @@
     [Crashlytics startWithAPIKey:[keysDict objectForKey:@"CrashlyticsAPIKey"]];
     return YES;
 }
-							
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    currentInstallation.channels = @[ @"global" ];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -41,7 +63,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
