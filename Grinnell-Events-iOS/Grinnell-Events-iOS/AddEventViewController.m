d7c0ba981c0038d422a1586ef0142c210f6c696f
Cherie Li <cherieli@Grinnells-MacBook-Air.local>
2019-03-06 09:23:37 -0600

    added text fields and textFieldShouldReturn

--- Grinnell-Events-iOS/Grinnell-Events-iOS/AddEventViewController.m
+++ Grinnell-Events-iOS/Grinnell-Events-iOS/AddEventViewController.m
@@ -11,6 +11,11 @@
 @interface AddEventViewController ()
 - (IBAction)returnButton:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UITextField *personName;
+@property (weak, nonatomic) IBOutlet UITextField *eventName;
+@property (weak, nonatomic) IBOutlet UITextField *clubName;
+@property (weak, nonatomic) IBOutlet UITextField *eventTimeDate;
+
 @end
 
 @implementation AddEventViewController
@@ -23,7 +28,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.personName.delegate = self;
+    self.eventName.delegate = self;
+    self.clubName.delegate = self;
+    self.eventTimeDate.delegate = self;
 }
+enum {
+    personNameTag = 0,
+    eventNameTag = 1,
+    clubNameTag = 2,
+    eventTimeDateTag = 3
+};
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    switch(textField.tag){
+        case personNameTag:
+            [_eventName becomeFirstResponder];
+            break;
+        case eventNameTag:
+            [_clubName becomeFirstResponder];
+            break;
+        case clubNameTag:
+            [_eventTimeDate becomeFirstResponder];
+            break;
+        default:
+            [_eventTimeDate resignFirstResponder];
+            
+    }
+    return true;
+}
+
+
 
 /*
 #pragma mark - Navigation
