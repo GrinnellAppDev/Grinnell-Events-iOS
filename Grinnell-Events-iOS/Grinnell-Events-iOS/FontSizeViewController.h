8df6bd8bb65804ccf3a16895ad6dbcaf5d09b2f7
Ivy Xu <ivyvyx116@gmail.com>
2019-04-14 14:57:04 -0500

    initialized view controller

---
+++ Grinnell-Events-iOS/Grinnell-Events-iOS/FontSizeViewController.h
@@ -0,0 +1,13 @@
+//
+//  FontSizeViewController.h
+//  Grinnell-Events-iOS
+//
+//  Created by 许文祺 on 4/14/19.
+//  Copyright © 2019 Grinnell AppDev. All rights reserved.
+//
+
+#ifndef FontSizeViewController_h
+#define FontSizeViewController_h
+
+
+#endif /* FontSizeViewController_h */
---
+++ Grinnell-Events-iOS/Grinnell-Events-iOS/FontSizeViewController.m
@@ -0,0 +1,9 @@
+//
+//  FontSizeViewController.m
+//  Grinnell-Events-iOS
+//
+//  Created by 许文祺 on 4/14/19.
+//  Copyright © 2019 Grinnell AppDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
